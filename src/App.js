b2d6c41e8e5d8a957ae56b9cce0c587087db2ca6
Amanbek Asylbekov <Amanbek_Asylbekov@epam.com>
2022-01-20 19:36:40 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react'; 
 import './App.css';
+import Header from './componets/header/Header.jsx';
+import Navbar from './componets/navbar/Navbar.jsx';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Main from './pages/main/Main.jsx';
+import Contacts from './pages/Contacts.jsx';
 
 function App() {
+  const [basket, setBasket] = useState(JSON.parse(localStorage.getItem("basket")) || [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Navbar basket={basket} />
+
+        <Switch>
+          <Route exact path="/">
+            <Main setBasket={setBasket} />
+          </Route>
+          <Route path="/contacts">
+            <Contacts />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
